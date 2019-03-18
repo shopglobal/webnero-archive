fbea8b8af330cc463504bc30d0851aada1926d7a
Mark Evans <president@worldvaporexpo.com>
2019-03-18 19:44:09 -0400

    syntax

--- js/etnx.js
+++ js/etnx.js
@@ -177,7 +177,7 @@ var ModelViewController = {
         const blockchainLink = coin==="etnx" ? 'blockexplorer.electronero.org' : coin==="etnxp" ? 'blockexplorer.electroneropulse.org' : '';
         const txidURL = '/tx/' + txid;
         const heightURL = '/block/' + height;
-        const operative = transaction=! false ? txidURL : heightURL;
+        const operative = transaction===true ? txidURL : heightURL;
         const blockchainExplorerURL = secureSocketLayer + blockchainLink + operative;
 
         return blockchainExplorerURL;
