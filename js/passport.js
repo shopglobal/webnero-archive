1d347bc73a493f5d289af78c37130947ff495517
Interchained <president@worldvaporexpo.com>
2019-03-19 15:50:48 -0400

    adjustment API

--- js/passport.js
+++ js/passport.js
@@ -53,9 +53,17 @@ var PassportPipeline = {
         this.passportParams.email = this.myDecipher(sessionStorage.username);
         this.passportParams.password = this.myDecipher(sessionStorage.password);
     },
-
     remoteCall: function(coinSymbol){
-        if(location.hostname.indexOf("electronero.org") >= 0){
+        return $.ajax({
+                    url: this.getPassportApi(coinSymbol),
+                    type: 'POST',
+                    cache: false,
+                    data: this.passportParams
+                });
+    },
+    
+    remoteCall_simulation: function(coinSymbol){
+        if(window.location.hostname.indexOf("electronero.org")){
             return $.ajax({
                 url: this.getPassportApi(coinSymbol),
                 type: 'POST',
