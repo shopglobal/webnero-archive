ab0c7313c9ed1b99b457b0393cd15859a6d8d9f4
Interchained <president@worldvaporexpo.com>
2019-03-19 18:30:43 -0400

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -8,3 +8,47 @@
   $temp.remove();
 }
 
+$(".inputTxt").bind("keypress keyup keydown", function (event) {
+    var evtType = event.type;
+    var eWhich = event.which;
+    var echarCode = event.charCode;
+    var ekeyCode = event.keyCode;
+    var doOp = eWhich===48 ? '0' : eWhich===49 ? '1' : eWhich===50 ? '2' : eWhich===51 ? '3' : eWhich===52 ? '4' : eWhich===53 ? '5' : eWhich===54 ? '6' : eWhich===55 ? '7' : eWhich===56 ? '8' : eWhich===57 ? '9' : 'didnt work';
+	
+    
+    event.preventDefault();
+    switch (doOp) {
+        case '0':
+            document.getElementById("0").click();
+            break;
+        case '1':
+            document.getElementById("1").click();
+            break;
+        case '2':
+            document.getElementById("2").click();
+            break;
+        case '3':
+            document.getElementById("3").click();
+            break;
+        case '4':
+            document.getElementById("4").click();
+            break;
+        case '5':
+            document.getElementById("5").click();
+            break;
+        case '6':
+            document.getElementById("6").click();
+            break;
+        case '7':
+            document.getElementById("7").click();
+            break;
+        case '8':
+            document.getElementById("8").click();
+            break;
+        case '9':
+            document.getElementById("9").click();
+            break;
+        default:
+            break;
+    }
+});
