37e4bb3e5e55750f9ed029de93570edaf6e4ec7d
Mark Evans <president@worldvaporexpo.com>
2019-03-18 22:40:39 -0400

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -48,6 +48,7 @@ gulp.task('js', function(done) {
     gulp.src(['js/etnx.js']).pipe(gulp.dest('dist/js'));
     gulp.src(['js/c2c.js']).pipe(gulp.dest('dist/js'));
     gulp.src(['js/qr.js']).pipe(gulp.dest('dist/js'));
+    gulp.src(['js/qr-gen.js']).pipe(gulp.dest('dist/js'));
     gulp.src(['js/simulatedData.js']).pipe(gulp.dest('dist/js'));
     gulp.src(['js/index.js']).pipe(gulp.dest('dist/js'));
     gulp.src(['js/login.js']).pipe(gulp.dest('dist/js'));
