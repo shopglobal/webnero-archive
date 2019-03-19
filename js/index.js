8058242816b1d64928ccd76338fd09d97be85485
Mark Evans <president@worldvaporexpo.com>
2019-03-18 20:40:40 -0400

    shipping

--- js/index.js
+++ js/index.js
@@ -3,16 +3,5 @@ $(function() {
 });
 
 $(document).ready(function(){
-
     ModelViewController.fillData();
-
-    // do something 
-    
-function copyToClipboard(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
-  document.execCommand("copy");
-  $temp.remove();
-}
 });
