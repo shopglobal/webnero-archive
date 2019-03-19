8bfeb9a18650431688128e24bc60749a7a1d9d93
Mark Evans <president@worldvaporexpo.com>
2019-03-19 02:34:05 -0400

    poco syntax

--- js/send.js
+++ js/send.js
@@ -57,7 +57,7 @@ $(document).on("click", "#send", function(){
         if(coin_selected != "etnxp-send"){
             operationData.uid = parseInt(myDecipher(sessionStorage.etnx_uuid))   
             console.log("etnx_uuid: "+myDecipher(sessionStorage.etnx_uuid))  
-            amountCoins = parseVal(ModelViewController.formatCoinTransaction(coinAmount,'etnxp'));
+            amountCoins = parseInt(ModelViewController.formatCoinTransaction(coinAmount,'etnxp'));
         }
 
         operationData.method = 'send_transaction';
