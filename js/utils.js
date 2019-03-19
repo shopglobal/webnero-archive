fb7c1f7b5a1f562d8b4d7995ebdefdd8f5f04c18
Interchained <president@worldvaporexpo.com>
2019-03-19 18:49:36 -0400

    modify the enterCase

--- js/utils.js
+++ js/utils.js
@@ -15,7 +15,14 @@ $("body").bind("keypress keyup keydown", function (event) {
     var ekeyCode = event.keyCode;
     var doOp = eWhich===48 ? '0' : eWhich===49 ? '1' : eWhich===50 ? '2' : eWhich===51 ? '3' : eWhich===52 ? '4' : eWhich===53 ? '5' : eWhich===54 ? '6' : eWhich===55 ? '7' : eWhich===56 ? '8' : eWhich===57 ? '9' : 'didnt work';
 	
-    
+	function enterCase(){
+    if(document.getElementById("pin-code")&&document.getElementById("login")){
+		    document.getElementById("pin-code").click();
+		    document.getElementById("login").click();
+	    } else if(document.getElementById("pin-code")){
+		    document.getElementById("pin-code").click();
+	    }
+	};
     event.preventDefault();
     switch (doOp) {
         case '0':
@@ -48,6 +55,12 @@ $("body").bind("keypress keyup keydown", function (event) {
         case '9':
             document.getElementById("9").click();
             break;
+        case 'enter':
+	    return enterCase();
+	    break;
+        case 'delete':
+	    document.getElementById("del").click();
+	    break;
         default:
             break;
     }
