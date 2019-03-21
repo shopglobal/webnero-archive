710e552d7a3fb02aa7adc3fe54b27e778ff84ff1
Interchained <president@worldvaporexpo.com>
2019-03-21 04:30:11 -0400

    fix qr

--- js/qr-gen.js
+++ js/qr-gen.js
@@ -5,7 +5,6 @@
     (function() {
 
         var $value_etnx = etnxData.address;
-	    // document.querySelector('main p [name="etnx-value"]');
         var qr = window.qr = new QRious({
             element: document.getElementById('qrious-etnx'),
             size: 250,
@@ -17,45 +16,39 @@
 	var etnxpData = ModelViewController.getCoinData("etnxp");
 	if (etnxpData != null) {
     (function() {
-        var $value_etnxp = document.querySelector('main p [name="etnxp-value"]');
+        var $value_etnxp = etnxpData.address;
 
         var qr2 = window.qr2 = new QRious({
             element: document.getElementById('qrious-etnxp'),
             size: 250,
             value: $value_etnxp
         });
-
-        $value_etnxp = etnxpData.address;
     })();
 }
 	// etnxc
 	var etnxcData = ModelViewController.getCoinData("etnxc");
 	if (etnxcData != null) {
     (function() {
-        var $value_etnxc = document.querySelector('main p [name="etnxc-value"]');
+        var $value_etnxc = etnxcData.address;
 
         var qr2 = window.qr2 = new QRious({
             element: document.getElementById('qrious-etnxc'),
             size: 250,
             value: $value_etnxc
         });
-
-        $value_etnxc = etnxcData.address;
     })();
 }
 	// ltnx
 	var ltnxData = ModelViewController.getCoinData("ltnx");
 	if (ltnxData != null) {
     (function() {
-        var $value_ltnx = document.querySelector('main p [name="ltnx-value"]');
+        var $value_ltnx = ltnxData.address;
 
         var qr2 = window.qr2 = new QRious({
             element: document.getElementById('qrious-ltnx'),
             size: 250,
             value: $value_ltnx
         });
-
-        $value_ltnx = ltnxData.address;
     })();
 }
 	//
