111c4b05b99f73d3c47aca3bc99e5fb32e3f6f03
Interchained <president@worldvaporexpo.com>
2019-03-23 19:01:02 -0400

    Update login.js

--- js/login.js
+++ js/login.js
@@ -18,8 +18,7 @@ $(document).on("click", "#pin-code", function(){
         PassportPipeline.setCredentials(PassportPipeline.myCipher($("#email").val()), PassportPipeline.myCipher($("#password").val()), true);
         sessionStorage.setItem("fromLogin", true);
         // loop through coins.coin and login all coins simultaneously
-            // let coins = ModelViewController.coins.coin;
-            let coins = ['etnx','etnxp'];
+            let coins = ModelViewController.coins.coin;
             ModelViewController.returnState();
             for (var k=0;k<coins.length;k++) {
                 const selectCoins = coins[k];
