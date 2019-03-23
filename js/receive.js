af0119dc2a533865dbc97f1fcc92eee2ae6dfdbb
Interchained <president@worldvaporexpo.com>
2019-03-23 17:56:03 -0400

    replace gqr with qrious on later qr calls

--- js/receive.js
+++ js/receive.js
@@ -6,6 +6,15 @@ $(document).on("click", ".coin-selector", function(){
 $(document).on("click", "blockquote", function(){
     $("blockquote").removeClass("selected");
     $(this).addClass("selected");
-
-    document.getElementById("qrimage").innerHTML="<img src='https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl="+encodeURIComponent($(this).children("p").html())+"'/>";
+    // replace google QR later with qrious as needed
+    (function() {
+        // example
+        var $value_qr = document.getElementById("qrimage").encodeURIComponent($(this).children("p").html());
+        var qr = window.qr = new QRious({
+            element: document.getElementById('qrimage'),
+            size: 250,
+            value: $value_qr
+        });
+    })();
+    //document.getElementById("qrimage").innerHTML="<img src='https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl="+encodeURIComponent($(this).children("p").html())+"'/>";
 });
