7a2e9c77988edc274465250fb66121258477357a
apaolinelli <alessandro.paolinelli@gmail.com>
2019-03-26 18:45:54 +0100

    Added confirm modal and other fields

--- js/settings.js
+++ js/settings.js
@@ -5,3 +5,21 @@ $(function() {
 $(document).ready(function(){
     
 });
+
+$(document).on("change", "input[type='checkbox']", function(){
+    
+    if(this.checked)
+        $("#confirm-msg").text($(this).attr("msg-on"));
+    else
+        $("#confirm-msg").text($(this).attr("msg-off"));
+    console.log($(this).attr("coin"));
+    $("#confirm-modal").modal();
+});
+
+$(document).on("click", "#confirm-ok", function(){
+    $("#confirm-modal").modal('hide');
+});
+
+$(document).on("click", "#confirm-canc", function(){
+    $("#confirm-modal").modal('hide');
+});
