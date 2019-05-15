42755c3c496ed9add5c502cbcabed4c73692f624
Alessandro Paolinelli <alessandro.paolinelli@gmail.com>
2019-05-15 23:15:28 +0200

    Fixed clean digit inputs

--- js/login.js
+++ js/login.js
@@ -1,4 +1,5 @@
 $(document).on("click", "#login", function(){
+    $(".inputDigit").val("");
     $("#pin-code-container").css("display", "block");
     $("#login-container").css("display", "none");
 });
@@ -18,13 +19,12 @@ $(document).on("click", "#pin-code", function(){
         PassportPipeline.setCredentials(PassportPipeline.myCipher($("#email").val()), PassportPipeline.myCipher($("#password").val()), true);
         sessionStorage.setItem("fromLogin", true);
         // loop through coins.coin and login all coins simultaneously
-            let coins = ModelViewController.coins.coin;
-            ModelViewController.returnState();
-            for (var k=0;k<coins.length;k++) {
-                const selectCoins = coins[k];
-                PassportPipeline.performOperation(selectCoins, ModelViewController.initCoin);
-            };
-        
+        let coins = ModelViewController.coins.coin;
+        ModelViewController.returnState();
+        for (var k=0;k<coins.length;k++) {
+            const selectCoins = coins[k];
+            PassportPipeline.performOperation(selectCoins, ModelViewController.initCoin);
+        };
     }
 });
 
