7760bb3f4cb9426063b27a10242cb7bdecf463e1
Mark Evans <president@worldvaporexpo.com>
2020-09-17 19:51:12 -0400

    updates

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,6 @@
 let lightMode =  sessionStorage.getItem("light-mode");
 if(lightMode == null){
-    lightMode = true;
+    lightMode = false;
     sessionStorage.setItem("light-mode", lightMode);
 }
 
