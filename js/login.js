56ce725ad97ade862c8a5d515e8d901c4dc4a33d
Mark Evans <president@worldvaporexpo.com>
2020-09-17 22:07:02 -0400

    update more login

--- js/login.js
+++ js/login.js
@@ -1,32 +1,32 @@
 
-$(document).on("click", "#login", function(){
-    cleanPinCode();
-    $("#pin-code-container").css("display", "block");
-    $("#login-container").css("display", "none");
-});
+// $(document).on("click", "#login", function(){
+//     cleanPinCode();
+//     $("#pin-code-container").css("display", "block");
+//     $("#login-container").css("display", "none");
+// });
 
-$(document).on("click", "#pin-code", function(){
-    $(".alert").css("display", "none");
-    if(pin_code.length < 5){
-        $(".alert-danger").html("Please provide 5 digits");
-        $(".alert-danger").css("display", "block");
-    }
-    else {
-        $(".alert").css("display", "none");
-        $("#spinner-modal").modal('show');
+// $(document).on("click", "#pin-code", function(){
+//     $(".alert").css("display", "none");
+//     if(pin_code.length < 5){
+//         $(".alert-danger").html("Please provide 5 digits");
+//         $(".alert-danger").css("display", "block");
+//     }
+//     else {
+//         $(".alert").css("display", "none");
+//         $("#spinner-modal").modal('show');
         
-        PassportPipeline.setCode(PassportPipeline.myCipher(pin_code));
-        PassportPipeline.setCredentials(PassportPipeline.myCipher($("#email").val()), PassportPipeline.myCipher($("#password").val()), true);
-        sessionStorage.setItem("fromLogin", true);
-        // loop through coins.coin and login all coins simultaneously
-        let coins = ModelViewController.coins.coin;
-        ModelViewController.returnState();
-        for (var k=0;k<coins.length;k++) {
-            const selectCoins = coins[k];
-            // fix login
-            PassportPipeline.performOperation("crfi", ModelViewController.initCoin);
-        };
-    }
-});
+//         PassportPipeline.setCode(PassportPipeline.myCipher(pin_code));
+//         PassportPipeline.setCredentials(PassportPipeline.myCipher($("#email").val()), PassportPipeline.myCipher($("#password").val()), true);
+//         sessionStorage.setItem("fromLogin", true);
+//         // loop through coins.coin and login all coins simultaneously
+//         let coins = ModelViewController.coins.coin;
+//         ModelViewController.returnState();
+//         for (var k=0;k<coins.length;k++) {
+//             const selectCoins = coins[k];
+//             // fix login
+//             PassportPipeline.performOperation("crfi", ModelViewController.initCoin);
+//         };
+//     }
+// });
 
 
