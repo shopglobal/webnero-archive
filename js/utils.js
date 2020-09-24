564649cf41dca982c5b065750e03d983024eb852
Interchained <evansmark.work@gmail.com>
2020-09-23 22:35:30 -0400

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -90,3 +90,21 @@ var Utils = {
         return regex.test(code);
     }
 }
+
+function loginFail(){
+    $("#pin-code-container").css("display", "none");
+    $("#login-container").css("display", "block");
+    
+    $(".alert-danger").css("display", "block");
+    $("#spinner-modal").modal('hide');
+}
+
+function loginCodeFail(){
+    $(".alert-danger").html("Wrong code");
+    $(".alert-danger").css("display", "block");
+    $("#spinner-modal").modal('hide');
+}
+
+function loginSuccess(){
+    $(".alert-success").css("display", "block");
+}
