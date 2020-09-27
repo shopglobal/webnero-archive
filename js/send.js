f17d4632896a46969cbce998a91776dc78458614
Interchained <evansmark.work@gmail.com>
2020-09-27 15:19:43 -0400

    test

--- js/send.js
+++ js/send.js
@@ -21,7 +21,7 @@ function sendCallback(coinSymbol){
 	coinSymbol = 'crfi';
     PassportPipeline.setMethod('send_transaction');
 	
-    const coinAmount = parseFloat(document.getElementById("amount").innerHTML);
+    const coinAmount = document.getElementById("amount");
     console.log("coinAmount: " + coinAmount);
     var amountOut = parseInt(ModelViewController.formatCoinTransaction(coinAmount, coinSymbol));
     console.log("amountOut: " + amountOut);
