ce68410ae7cbd06ce67b582d68fba05f89740ded
Interchained <evansmark.work@gmail.com>
2020-10-01 19:28:03 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,20 +3,12 @@ $(function() {
 });
 
 $(document).ready(function(){
-
     if(!PassportPipeline.hasValidSession()){ 
         location.href = "login.html";
     } else if(sessionStorage.fromLogin == "true"){
         sessionStorage.setItem("fromLogin", false);
         ModelViewController.fillData();
-        var refresh;
-        function autoRefresh() {
-          ModelViewController.refreshData;
-        }  
-        (function refreshWallet(){
-          refresh = setInterval(autoRefresh, 7500);
-        }
-        })();
+        setInterval(ModelViewController.refreshData, 7500);
     } else {
         ModelViewController.refreshData();
     }
