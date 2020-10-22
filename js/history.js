e71effb042aed72290a197213554950450c59b07
Interchained <evansmark.work@gmail.com>
2020-10-22 00:33:46 -0400

    Update history.js

--- js/history.js
+++ js/history.js
@@ -6,14 +6,11 @@ $(document).ready(function() {
     if(!PassportPipeline.hasValidSession()){ 
         location.href = "login.html";
     } else {
-        sessionStorage.setItem("fromLogin", false);
-        ModelViewController.fillHistory();
         setInterval(ModelViewController.fillHistory, 15000);
-        $('#transaction-history').DataTable({
+    };
+    
+    $('#transaction-history').DataTable({
         responsive: true,
-        paging: false,
         "order": [[ 3, 'desc' ]]
     });
-    };
-
 });
