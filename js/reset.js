11b0c189be0697cdb58c9716913efd57a2ecf613
Interchained <evansmark.work@gmail.com>
2020-10-25 14:17:36 -0400

    resetFail

--- js/reset.js
+++ js/reset.js
@@ -1,3 +1,11 @@
+function resetFail(){
+    $("#pin-code-container").css("display", "none");
+    $("#login-container").css("display", "block");
+    
+    $(".alert-danger").css("display", "block");
+    $("#spinner-modal").modal('hide');
+}
+
 $(document).on("click", "#reset", function(){
     PassportPipeline.resetPassword('crfi', $("#email").val())
     $("#pin-code-container").css("display", "block");
