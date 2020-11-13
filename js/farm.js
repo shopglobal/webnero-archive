3320c54bb3119924ae487497cc6cc23cb1b67b00
Interchained <evansmark.work@gmail.com>
2020-11-12 23:26:53 -0500

    Update farm.js

--- js/farm.js
+++ js/farm.js
@@ -1,1 +1,18 @@
 // farm.js -- FARM BTC, ETH, USDT, USDC, DAI, etc etc etc to EARN CRFI rewards
+
+function farm() {
+  var x = document.getElementById("farm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function claim() {
+  var x = document.getElementById("claim");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
