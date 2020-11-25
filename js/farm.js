3ea9573e054e5a2357c667bd9ac01994e0ab62fa
Interchained <evansmark.work@gmail.com>
2020-11-25 17:31:15 -0500

    swap claim/farm not both

--- js/farm.js
+++ js/farm.js
@@ -1,18 +1,25 @@
 // farm.js -- FARM BTC, ETH, USDT, USDC, DAI, etc etc etc to EARN CRFI rewards
+var x = document.getElementById("claim");
 
 function farm() {
   var x = document.getElementById("farm");
+  var y = document.getElementById("claim");
   if (x.style.display === "none") {
-    x.style.display = "block";
+    y.style.display = "none";
+    x.style.display = "block";    
   } else {
+    y.style.display = "block";
     x.style.display = "none";
   }
 }
 function claim() {
   var x = document.getElementById("claim");
+  var y = document.getElementById("farm");
   if (x.style.display === "none") {
+    y.style.display = "none";
     x.style.display = "block";
-  } else {
+  } else {    
+    y.style.display = "block";
     x.style.display = "none";
   }
 }
