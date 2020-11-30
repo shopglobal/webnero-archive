953e61314c756103def6eaff863e0c6215f17a3a
Interchained <evansmark.work@gmail.com>
2020-11-30 14:20:43 -0500

    Update reset.js

--- js/reset.js
+++ js/reset.js
@@ -18,6 +18,25 @@ function resetSuccess(){
     $("#spinner-modal").modal('hide');
 }
 
+$('#enable-settings').on('change.bootstrapSwitch', function(e) {
+PassportPipeline.getUUkey('crfi');
+setTimeout(PassportPipeline.logUU(), 3000);
+});
+
+$(document).on("click", "#resetpwd", function(){
+    if($("#pass").val() != $("#repeat").val()){
+        let userErr = "Your password does not match. Please correct your password, and try again momentarily.";
+        $(".alert-danger").html(userErr)
+        resetFail();
+        return;
+    }
+    else {
+    PassportPipeline.resetPassword('crfi', $("#email").val(), $("#pass").val(), key_set)
+    //$("#pin-code-container").css("display", "block");
+    //$("#reset-container").css("display", "none");
+    }
+});
+
 $(document).on("click", "#reset", function(){
     if($("#email").val() != $("#confirm-email").val() || $("#confirm-email").val() != $("#email").val()){
         let userErr = "Your email does not match. Please correct your email, and try again momentarily.";
