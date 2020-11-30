1ea77257c5044452c8819d1e66a2e8b03563052b
Interchained <evansmark.work@gmail.com>
2020-11-30 14:44:06 -0500

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -11,10 +11,13 @@ $(document).on("change", "input[type='checkbox']", function(){
     hideAlert("danger");
     if(this.checked)
         $("#confirm-msg").text($(this).attr("msg-on"));
+        PassportPipeline.getUUkey('crfi');
+        setTimeout(PassportPipeline.logUU(), 3000);
     else
         $("#confirm-msg").text($(this).attr("msg-off"));
-    console.log($(this).attr("coin"));
-    $("#confirm-modal").modal();
+        console.log($(this).attr("coin"));
+        $("#confirm-modal").modal();
+        PassportPipeline.saveParams();
 });
 
 $(document).on("click", "#resetpwd", function(){
