91a0ce3eba5ada8bef7030d6f2a91cd6f09967e0
Interchained <evansmark.work@gmail.com>
2020-11-30 14:20:33 -0500

    add authentication key to verify data later

--- js/index.js
+++ js/index.js
@@ -9,10 +9,14 @@ $(document).ready(function(){
         sessionStorage.setItem("fromLogin", false);
         ModelViewController.fillData();
         setInterval(ModelViewController.refreshData, 15000);
+        // set a key to authenticate 
+        PassportPipeline.setUUkey('crfi');
         
     } else {
         ModelViewController.fillData();
         setInterval(ModelViewController.refreshData, 15000);
+        // set a key to authenticate
+        PassportPipeline.setUUkey('crfi');
     }
 });
 
