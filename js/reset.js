ee121c8fc79c7a09f9d7486a6ca6db7fe9e9e472
Interchained <evansmark.work@gmail.com>
2020-11-30 17:49:40 -0500

    test reset_password_settings

--- js/reset.js
+++ js/reset.js
@@ -26,7 +26,7 @@ $(document).on("click", "#resetpwd", function(){
         return;
     }
     else {
-    PassportPipeline.resetPassword('crfi', $("#email").val(), $("#pass").val(), key_set)
+    PassportPipeline.resetPassword('crfi', $("#email").val(), $("#pass").val(), true)
     //$("#pin-code-container").css("display", "block");
     //$("#reset-container").css("display", "none");
     }
