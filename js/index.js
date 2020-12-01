cb84d1212d40be8a45de11df5b12f13af34d6fff
Interchained <evansmark.work@gmail.com>
2020-11-30 22:57:00 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,17 +6,20 @@ $(document).ready(function(){
     if(!PassportPipeline.hasValidSession()){ 
         location.href = "login.html";
     } else if(sessionStorage.fromLogin == "true"){
+        
         sessionStorage.setItem("fromLogin", false);
         ModelViewController.fillData();
         setInterval(ModelViewController.refreshData, 15000);
         // set a key to authenticate 
         PassportPipeline.setUUkey('crfi');
         
+        
     } else {
         ModelViewController.fillData();
         setInterval(ModelViewController.refreshData, 15000);
         // set a key to authenticate
         PassportPipeline.setUUkey('crfi');
+        
     }
 });
 
