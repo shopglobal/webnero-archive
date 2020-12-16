a1b52efc8dd69854307d484a4448aaec8425ed48
Interchained <evansmark.work@gmail.com>
2020-12-16 18:23:25 -0500

    Update crystalID.js

--- js/crystalID.js
+++ js/crystalID.js
@@ -37,6 +37,35 @@ $(function() {
     }
 });
 
+(function($) {
+    $.fn.flash_message = function(options) {
+      
+      options = $.extend({
+        text: 'Done',
+        time: 1000,
+        how: 'before',
+        class_name: ''
+      }, options);
+      
+      return $(this).each(function() {
+        if( $(this).parent().find('.flash_message').get(0) )
+          return;
+        
+        var message = $('<span />', {
+          'class': 'flash_message ' + options.class_name,
+          text: options.text
+        }).hide().fadeIn('fast');
+        
+        $(this)[options.how](message);
+        
+        message.delay(options.time).fadeOut('normal', function() {
+          $(this).remove();
+        });
+        
+      });
+    };
+})(jQuery);
+
 $(document).on("click", ".coin-selector", function(){
     if(!$(this).hasClass("btn-selected")){
         $(".coin-selector").removeClass("btn-selected");
