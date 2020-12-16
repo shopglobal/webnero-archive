68a2def9a16b78739fcf66fcb95422925171d9c0
Interchained <evansmark.work@gmail.com>
2020-12-16 18:50:40 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,16 @@ $(function() {
 });
 
 $(document).ready(function(){
+    let message;
+    function statusMessage(message){
+            $('#status-area').flash_message({
+            text: message,
+            how: 'append'
+            });
+    };
+    function statusUpdate(message){
+            setInterval(statusMessage(message), 10000);     
+    };
     if(!PassportPipeline.hasValidSession()){ 
         location.href = "login.html";
     } else if(sessionStorage.fromLogin == "true"){
@@ -10,10 +20,14 @@ $(document).ready(function(){
         sessionStorage.setItem("fromLogin", false);
         setInterval(ModelViewController.refreshData, 60000);
         // set a key to authenticate crystalID
-        PassportPipeline.setUUkey('crfi');        
+        PassportPipeline.setUUkey('crfi');   
+        message = 'Folio Updated!';
+        setTimeout(statusUpdate(message), 1337);
     } else {
         ModelViewController.fillData();
         setInterval(ModelViewController.refreshData, 60000);
+        message = 'Folio Updated!';
+        setTimeout(statusUpdate(message), 1337);
     }
 });
 
