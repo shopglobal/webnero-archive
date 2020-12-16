30dd289ee96ab6c6e763caba8ce6b90bff1db308
Interchained <evansmark.work@gmail.com>
2020-12-16 16:27:21 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,36 +6,14 @@ $(document).ready(function(){
     if(!PassportPipeline.hasValidSession()){ 
         location.href = "login.html";
     } else if(sessionStorage.fromLogin == "true"){
-        
         ModelViewController.fillData();
-        var l;
-        console.log("local storage: ");
-        for (l = 0; l < localStorage.length; l++)   {
-            let crfiData = localStorage.getItem(localStorage.key(l));
-            console.log(crfiData);
-            console.log("address: " + crfiData.address);
-            console.log("balance: " + crfiData.balance);
-        }
         sessionStorage.setItem("fromLogin", false);
         setInterval(ModelViewController.refreshData, 60000);
-        // set a key to authenticate 
-        PassportPipeline.setUUkey('crfi');
-        
-        
+        // set a key to authenticate crystalID
+        PassportPipeline.setUUkey('crfi');        
     } else {
-        var m;
-        console.log("local storage: ");
-         for (m = 0; m < localStorage.length; m++)   {
-            let crfiData = localStorage.getItem(localStorage.key(m));
-            console.log(crfiData);
-            console.log("address: " + crfiData.address);
-            console.log("balance: " + crfiData.balance);
-        }
         ModelViewController.fillData();
         setInterval(ModelViewController.refreshData, 60000);
-        // set a key to authenticate
-        PassportPipeline.setUUkey('crfi');
-        
     }
 });
 
