bc2bfe1267eee2191e6252ae255561acfa3fc962
Interchained <evansmark.work@gmail.com>
2020-12-16 22:06:19 -0500

    convert babel

--- js/locals.js
+++ js/locals.js
@@ -24,8 +24,7 @@ for (m = 0; m < sessionStorage.length; m++) {
 })();
 
 
-$(function() {
-const setupExamples = () => {
+function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}const setupExamples = () => {
   // -----------------
   // with border -----
   // -----------------
@@ -39,8 +38,8 @@ const setupExamples = () => {
     wavesAlpha: 0.8,
     waves: "https://cdn.rawgit.com/av-dev/noise-button/930cbd38/Z3hB7It.png",
     displacementMap:
-      "https://cdn.rawgit.com/av-dev/noise-button/930cbd38/displace-map.jpeg"
-  });
+    "https://cdn.rawgit.com/av-dev/noise-button/930cbd38/displace-map.jpeg" });
+
 
   // -----------------
   // without border --
@@ -50,8 +49,8 @@ const setupExamples = () => {
     wavesPos: { x: 0, y: 0.3 },
     polygon: "30, 0, 30, 0",
     backgroundColor: "0xFFFFFF",
-    backgroundAlpha: 0
-  });
+    backgroundAlpha: 0 });
+
 };
 
 class NoiseButton extends PIXI.Application {
@@ -59,123 +58,123 @@ class NoiseButton extends PIXI.Application {
     super({
       autoStart: false,
       autoResize: true,
-      transparent: true
-    });
+      transparent: true });_defineProperty(this, "resize",
 
-    this.options = Object.assign(
-      {
-        backgroundColor: 0x4875cc,
-        backgroundAlpha: 1,
-        polygon: "0, 0, 0, 0",
-        borderColor: 0x4875cc,
-        borderWidth: 0,
-        wavesAlpha: 1,
-        displacementScale: { x: 30, y: 50 },
-        displacementMap: "http://digitalfreepen.com/images/2017/whitenoise.png"
-      },
-      options
-    );
 
-    // example of the received polygon string
-    // '30, 0, 30, 0'
-    this.polygon = this.options.polygon
-      .replace(/\s/g, "")
-      .split(",")
-      .map(el => {
-        const number = el | 0;
-        return number > this.options.borderWidth
-          ? number - this.options.borderWidth / 2
-          : number;
-      });
 
-    this.offset = 20;
-    this.animate = false;
-    return this.createCanvas();
-  }
 
-  async createCanvas() {
-    this.options.element.classList.add("noise-container");
-    this.view.classList.add("noise-canvas");
-    this.options.element.appendChild(this.view);
 
-    this.container = new PIXI.Container();
-    this.stage.addChild(this.container);
 
-    if(this.options.waves) {
-      const wavesTexture = await this.loadTexture(this.options.waves);
-      this.waves = new PIXI.Sprite(wavesTexture);
-    }
-    
-    this.noiseSprite = PIXI.Sprite.fromImage(this.options.displacementMap);
 
-    this.setSize();
-    this.addGraphics();
-    this.bindEvents();
-    this.render();
-    this.options.element.classList.add("canvas-ready");
-  }
-  static debounce(func, context, wait, immediate) {
-    let timeout;
 
-    return () => {
-      const args = arguments;
 
-      const later = () => {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
 
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-  }
-  addGraphics() {
-    this.container.addChild(this.getPolygon(true));
 
-    if (this.options.waves) this.drawWaves();
 
-    const rect = new PIXI.Graphics();
-    rect.beginFill(0, 0);
-    rect.drawRect(0, 0, this.width, this.width);
 
-    this.container.addChild(rect);
-    this.container.addChild(this.getPolygon());
-    this.setMask();
-    this.addFilter();
-  }
 
-  setMask() {
-    let mask = this.getPolygon();
-    this.stage.addChild(mask);
-    this.container.mask = mask;
-  }
 
-  drawWaves() {
-    this.waves.alpha = 1 - this.options.wavesAlpha;
 
-    this.waves.y = this.height * this.options.wavesPos.y;
-    this.container.addChild(this.waves);
 
-    this.waves.width = this.waves.height = this.width;
-  }
 
-  setSize() {
-    const parentWidth = this.options.element.offsetWidth;
-    const parentHeight = this.options.element.offsetHeight;
 
-    this.width = parentWidth + this.offset * 2;
-    this.height = parentHeight + this.offset * 2;
 
-    if (this.oldWidth !== this.width) {
-      this.renderer.resize(this.width, this.height);
-      this.oldWidth = this.width;
-      return true;
-    } else return false;
-  }
 
-  resize = NoiseButton.debounce(
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    NoiseButton.debounce(
     async () => {
       if (this.setSize()) {
         this.container.removeChildren(0, this.container.children.length - 1);
@@ -186,9 +185,9 @@ class NoiseButton extends PIXI.Application {
       }
     },
     this,
-    100
-  );
-
+    100));this.options = Object.assign({ backgroundColor: 0x4875cc, backgroundAlpha: 1, polygon: "0, 0, 0, 0", borderColor: 0x4875cc, borderWidth: 0, wavesAlpha: 1, displacementScale: { x: 30, y: 50 }, displacementMap: "http://digitalfreepen.com/images/2017/whitenoise.png" }, options); // example of the received polygon string
+    // '30, 0, 30, 0'
+    this.polygon = this.options.polygon.replace(/\s/g, "").split(",").map(el => {const number = el | 0;return number > this.options.borderWidth ? number - this.options.borderWidth / 2 : number;});this.offset = 20;this.animate = false;return this.createCanvas();}async createCanvas() {this.options.element.classList.add("noise-container");this.view.classList.add("noise-canvas");this.options.element.appendChild(this.view);this.container = new PIXI.Container();this.stage.addChild(this.container);if (this.options.waves) {const wavesTexture = await this.loadTexture(this.options.waves);this.waves = new PIXI.Sprite(wavesTexture);}this.noiseSprite = PIXI.Sprite.fromImage(this.options.displacementMap);this.setSize();this.addGraphics();this.bindEvents();this.render();this.options.element.classList.add("canvas-ready");}static debounce(func, context, wait, immediate) {let timeout;return () => {const args = arguments;const later = () => {timeout = null;if (!immediate) func.apply(context, args);};const callNow = immediate && !timeout;clearTimeout(timeout);timeout = setTimeout(later, wait);if (callNow) func.apply(context, args);};}addGraphics() {this.container.addChild(this.getPolygon(true));if (this.options.waves) this.drawWaves();const rect = new PIXI.Graphics();rect.beginFill(0, 0);rect.drawRect(0, 0, this.width, this.width);this.container.addChild(rect);this.container.addChild(this.getPolygon());this.setMask();this.addFilter();}setMask() {let mask = this.getPolygon();this.stage.addChild(mask);this.container.mask = mask;}drawWaves() {this.waves.alpha = 1 - this.options.wavesAlpha;this.waves.y = this.height * this.options.wavesPos.y;this.container.addChild(this.waves);this.waves.width = this.waves.height = this.width;}setSize() {const parentWidth = this.options.element.offsetWidth;const parentHeight = this.options.element.offsetHeight;this.width = parentWidth + this.offset * 2;this.height = parentHeight + this.offset * 2;if (this.oldWidth !== this.width) {this.renderer.resize(this.width, this.height);this.oldWidth = this.width;return true;} else return false;}
   loadTexture(src) {
     return new Promise(resolve => {
       const loader = new PIXI.loaders.Loader();
@@ -210,26 +209,26 @@ class NoiseButton extends PIXI.Application {
 
   createTimeLine() {
     this.timeline = new TimelineMax({
-      onUpdate: this::this.render,
+      onUpdate: this.render.bind(this),
       paused: true,
-      onStart: () => (this.animate = true),
-      onComplete: () => (this.animate = false)
-    })
+      onStart: () => this.animate = true,
+      onComplete: () => this.animate = false }).
 
-      .to(this.noiseFilter.scale, 0.2, {
-        x: this.options.displacementScale.x,
-        y: this.options.displacementScale.y
-      })
 
-      .fromTo(
-        this.noiseSprite,
-        0.5,
-        { x: -(this.noiseSprite.width * 0.66) },
-        { x: 0 },
-        "-=.2"
-      )
+    to(this.noiseFilter.scale, 0.2, {
+      x: this.options.displacementScale.x,
+      y: this.options.displacementScale.y }).
 
-      .to(this.noiseFilter.scale, 0.2, { x: 0, y: 0 }, "-=.2");
+
+    fromTo(
+    this.noiseSprite,
+    0.5,
+    { x: -(this.noiseSprite.width * 0.66) },
+    { x: 0 },
+    "-=.2").
+
+
+    to(this.noiseFilter.scale, 0.2, { x: 0, y: 0 }, "-=.2");
   }
 
   play() {
@@ -238,8 +237,8 @@ class NoiseButton extends PIXI.Application {
 
   bindEvents() {
     this.createTimeLine();
-    this.options.element.addEventListener("mouseenter", this::this.play);
-    window.addEventListener("resize", this::this.resize);
+    this.options.element.addEventListener("mouseenter", this.play.bind(this));
+    window.addEventListener("resize", this.resize.bind(this));
   }
 
   getPolygon(background) {
@@ -252,31 +251,31 @@ class NoiseButton extends PIXI.Application {
     graphics.position.y = this.offset + this.options.borderWidth / 2;
 
     const arrayLines = [
-      [0, points[0]],
-      [points[0], 0],
-      [width - points[1], 0],
-      [width, points[1]],
-      [width, height - points[2]],
-      [width - points[2], height],
-      [points[3], height],
-      [0, height - points[3]],
-      [0, points[0]]
-    ];
+    [0, points[0]],
+    [points[0], 0],
+    [width - points[1], 0],
+    [width, points[1]],
+    [width, height - points[2]],
+    [width - points[2], height],
+    [points[3], height],
+    [0, height - points[3]],
+    [0, points[0]]];
+
 
     graphics.lineStyle(this.options.borderWidth, this.options.borderColor);
 
     graphics.beginFill(
-      this.options.backgroundColor,
-      background ? 1 - this.options.backgroundAlpha : 0
-    );
+    this.options.backgroundColor,
+    background ? 1 - this.options.backgroundAlpha : 0);
 
-    for (let i = 0, prevCoords = []; i < arrayLines.length; i++) {
+
+    for (let i = 0, prevCoords = []; i < arrayLines.length; i++) {if (window.CP.shouldStopExecution(0)) break;
       if (
-        prevCoords.length &&
-        prevCoords[0] === arrayLines[i][0] &&
-        prevCoords[1] === arrayLines[i][1]
-      )
-        continue;
+      prevCoords.length &&
+      prevCoords[0] === arrayLines[i][0] &&
+      prevCoords[1] === arrayLines[i][1])
+
+      continue;
       if (i === 0) {
         graphics.moveTo(arrayLines[i][0], arrayLines[i][1]);
         prevCoords = arrayLines[i];
@@ -285,13 +284,12 @@ class NoiseButton extends PIXI.Application {
 
       prevCoords = arrayLines[i];
       graphics.lineTo(arrayLines[i][0], arrayLines[i][1]);
-    }
+    }window.CP.exitedLoop(0);
 
     graphics.endFill();
 
     return graphics;
-  }
-}
+  }}
 
-setupExamples()
-});
+
+setupExamples();
