893d6a0e30fc80a3b578c9f41532d0cbaee1f092
Interchained <evansmark.work@gmail.com>
2020-12-18 17:52:06 -0500

    Update stake.js

--- js/stake.js
+++ js/stake.js
@@ -70,7 +70,7 @@ document.getElementById("quote-stake").addEventListener("click", function() {
 	  quoteMinusFee = total - performanceFee;
 	  collateral_value = amountToQuote;
 	  document.getElementById("est_performance_value").innerHTML = performanceFee + "CRFI";
-	  document.getElementById("quoted_value").innerHTML = varRateA * 10 + "%";
+	  document.getElementById("quoted_value").innerHTML = varRateA * 100 + "%";
 	  document.getElementById("est_reward_value").innerHTML = est + "CRFI";
 	  document.getElementById("est_return_value").innerHTML = quoteMinusFee + " CRFI";
 	  document.getElementById("collateral_value").innerHTML = collateral_value + " CRFI";
@@ -83,7 +83,7 @@ document.getElementById("quote-stake").addEventListener("click", function() {
 	  quoteMinusFee = total - performanceFee;
 	  collateral_value = amountToQuote;
 	  document.getElementById("est_performance_value").innerHTML = performanceFee + "CRFI";
-	  document.getElementById("quoted_value").innerHTML = varRateB * 10 + "%";
+	  document.getElementById("quoted_value").innerHTML = varRateB * 100 + "%";
 	  document.getElementById("est_reward_value").innerHTML = est + "CRFI";
 	  document.getElementById("est_return_value").innerHTML = quoteMinusFee + " CRFI";
 	  document.getElementById("collateral_value").innerHTML = collateral_value + " CRFI";
@@ -96,7 +96,7 @@ document.getElementById("quote-stake").addEventListener("click", function() {
 	  quoteMinusFee = total - performanceFee;
 	  collateral_value = amountToQuote;
 	  document.getElementById("est_performance_value").innerHTML = performanceFee + "CRFI";
-	  document.getElementById("quoted_value").innerHTML = varRateC * 10 + "%";
+	  document.getElementById("quoted_value").innerHTML = varRateC * 100 + "%";
 	  document.getElementById("est_reward_value").innerHTML = est + "CRFI";
 	  document.getElementById("est_return_value").innerHTML = quoteMinusFee + " CRFI";
 	  document.getElementById("collateral_value").innerHTML = collateral_value + " CRFI";
@@ -109,7 +109,7 @@ document.getElementById("quote-stake").addEventListener("click", function() {
 	  quoteMinusFee = total - performanceFee;
 	  collateral_value = amountToQuote;
 	  document.getElementById("est_performance_value").innerHTML = performanceFee + "CRFI";
-	  document.getElementById("quoted_value").innerHTML = varRateD * 10 + "%";
+	  document.getElementById("quoted_value").innerHTML = varRateD * 100 + "%";
 	  document.getElementById("est_reward_value").innerHTML = est + "CRFI";
 	  document.getElementById("est_return_value").innerHTML = quoteMinusFee + " CRFI";
 	  document.getElementById("collateral_value").innerHTML = collateral_value + " CRFI";
@@ -122,7 +122,7 @@ document.getElementById("quote-stake").addEventListener("click", function() {
 	  quoteMinusFee = total - performanceFee;
 	  collateral_value = amountToQuote;
 	  document.getElementById("est_performance_value").innerHTML = performanceFee + "CRFI";
-	  document.getElementById("quoted_value").innerHTML = varRateE * 10 + "%";
+	  document.getElementById("quoted_value").innerHTML = varRateE * 100 + "%";
 	  document.getElementById("est_reward_value").innerHTML = est + "CRFI";
 	  document.getElementById("est_return_value").innerHTML = quoteMinusFee + " CRFI";
 	  document.getElementById("collateral_value").innerHTML = collateral_value + " CRFI";
@@ -135,7 +135,7 @@ document.getElementById("quote-stake").addEventListener("click", function() {
 	  quoteMinusFee = total - performanceFee;
 	  collateral_value = amountToQuote;
 	  document.getElementById("est_performance_value").innerHTML = performanceFee + "CRFI";
-	  document.getElementById("quoted_value").innerHTML = varRateF * 10 + "%";
+	  document.getElementById("quoted_value").innerHTML = varRateF * 100 + "%";
 	  document.getElementById("est_reward_value").innerHTML = est + "CRFI";
 	  document.getElementById("est_return_value").innerHTML = quoteMinusFee + " CRFI";
 	  document.getElementById("collateral_value").innerHTML = collateral_value + " CRFI";
